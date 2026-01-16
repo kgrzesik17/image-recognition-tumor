12b4a13076f65c3ba7d97af2bf2ffda9d8dcfa75
Kacper <kgrzesik17@gmail.com>
2026-01-16 10:53:20 +0100

    connected to data

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ from keras.models import Sequential
 from keras.utils import plot_model
 from keras.metrics import CategoricalAccuracy, TruePositives, FalsePositives, FalseNegatives, Accuracy
 import tensorflow_datasets as tfds
+
+directory_train = 'BrainTumorDataset/train'
+directory_test = 'BrainTumorDataset/test'
+
+positive_train = os.path.join(directory_train, 'yes')
+negative_train = os.path.join(directory_train, 'no')
+positive_test = os.path.join(directory_test, 'yes')
+negative_test = os.path.join(directory_test, 'no')
+
+print('Zbiór uczący')
+print(f'Liczba wyników pozytywnych {len(os.listdir(positive_train))}')
+print(f'Liczba wyników negatywnych {len(os.listdir(negative_train))}')
+
+print('Zbiór testowy')
+print(f'Liczba wyników pozytywnych {len(os.listdir(positive_test))}')
+print(f'Liczba wyników negatywnych {len(os.listdir(negative_test))}')
